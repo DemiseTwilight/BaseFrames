bd3c4146c31585933ad3f5823c5e62356c9c6733
DemiseTwilight <lizijie7471619@outlook.com>
2020-05-14 00:13:31 +0800

    重新提交

---
+++ Managers/CoroutineManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 协程管理 :用于非继承MonoBehaviour对象委托协程
+/// </summary>
+public class CoroutineManager : SingletonBase<CoroutineManager>
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    ///延时
+    public void Invoke(Action method, float waitSeconds)
+    {
+        if (waitSeconds < 0 || method == null)
+        {
+            return;
+        }
+        StartCoroutine(RunLaterCoroutine(method,waitSeconds));
+    }
+
+    IEnumerator RunLaterCoroutine(Action method, float waitSeconds)
+    {
+        yield return new WaitForSeconds(waitSeconds);
+        method();
+    }
+    /// <summary>
+    /// 重复
+    /// </summary>
+    /// <param name="method"></param>
+    /// <param name="number">次数</param>
+    /// <param name="periodically">周期</param>
+    public void Repeat(Action method,int number, float period)
+    {
+        if (number < 0 || method == null)
+        {
+            return;
+        }
+        StartCoroutine(RunRepeatCoroutine(method,number, period));
+    }
+
+    IEnumerator RunRepeatCoroutine(Action method,int number, float period)
+    {
+        for(int i = 0; i < number; i++)
+        {
+            method();
+            yield return new WaitForSeconds(period);
+        }
+        
+    }
+}
---
+++ Managers/ObjectPool/ReusableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * 可重用对象
+ * 可通过继承该对象来实现可重用对象的初始化及析构化
+ * */
+public abstract class ReusableObject : MonoBehaviour, IReusable
+
+{
+
+    public abstract void Spawn();
+
+    public abstract void UnSpawn();
+
+}
